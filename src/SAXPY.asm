2854237a3dc5d7f69348196b5c164f8424107f0a
svcheng <samuel.vincent.cheng@gmail.com>
2024-03-27 23:43:07 +0800

    implemented function in C and x86-64

--- src/SAXPY.asm
+++ src/SAXPY.asm
@@ -4,5 +4,30 @@ default rel
 
 global SAXPYasm
 SAXPYasm:
-
+    PUSH RBP
+    MOV RBP, RSP
+    ADD RBP, 16
+    
+    ; ECX - n
+    ; XMM1 - A
+    ; R8 - X
+    ; R9 - Y  
+    MOV RAX, [RBP+32] ; Z
+    
+    L1:       
+        VMULSS XMM4, XMM1, dword [R8]   ; XMM4 = A * X[i]
+        ADDSS XMM4, dword [R9]          ; XMM4 += Y[i]
+        MOVSS [RAX], XMM4               ; Z[i] = XMM4
+        
+        ADD R8, 4
+        ADD R9, 4
+        ADD RAX, 4
+        
+        DEC ECX
+        JZ FIN
+        JMP L1
+        
+    FIN:
+        POP RBP
+        
     ret
--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
-extern SAXPYasm(int n, float A, float* X, float* Y, float* Z);
+extern void SAXPYasm(int n, float A, float* X, float* Y, float* Z);
+
+void printZ(int n, float* Z) {
+	printf("Z = ");
+	for (int i = 0; i < n; i++) {
+		if (i < n - 1)
+			printf("%.2f, ", Z[i]);
+		else
+			printf("%.2f\n", Z[i]);
+	}
+}
+
+void SAXPY(int n, float A, float* X, float* Y, float* Z) {
+	for (int i = 0; i < n; i++) {
+		Z[i] = (A * X[i]) + Y[i];
+	}
+}
+
 int main() {
+	int n = 3;
+	float A = 2.0;
+
+	float X[3] = { 1.0, 2.0, 3.0 };
+	float Y[3] = { 11.0, 12.0, 13.0 };
+	float Z[3] = { 0.0, 0.0, 0.0 };
+
+	// Call C function
+	SAXPY(n, A, X, Y, Z);
+	printZ(n, Z);
+
+	for (int i = 0; i < n; i++)
+		Z[i] = 0.0;
+
+	SAXPYasm(n, A, X, Y, Z);
+	//printf("%.2f", *SAXPYasm(n, A, X, Y, Z));
+	printZ(n, Z);
+
 	return 0;
 }
