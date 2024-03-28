45cb9090b77301cdd3e8f89ce733214e91ce8be9
svcheng <samuel.vincent.cheng@gmail.com>
2024-03-28 14:30:37 +0800

    added test case, refactored

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h> 
 
 int numTestRuns = 30;
 
 extern void SAXPYasm(int n, float A, float* X, float* Y, float* Z);
 
-void printZ(int n, float* Z) {
-	for (int i = 0; i < n; i++) {
+void printFirstTen(float* Z, int n) {
+	int end = n < 10 ? n : 10;
+	for (int i = 0; i < end; i++) {
 		printf("%.2f  ", Z[i]);
 	}
 }
@@ -28,25 +30,44 @@ void SAXPY(int n, float A, float* X, float* Y, float* Z) {
 
 double benchmarkC(int n, float A, float* X, float* Y, float* Z) {
 	printf("C     : ");
+
 	clock_t time = clock();
 	SAXPY(n, A, X, Y, Z);
 	time = clock() - time;
-	double timeElapsed = (double)time / CLOCKS_PER_SEC;
-	printf("|  Time Elapsed: %.4f\n", timeElapsed);
 
-	return timeElapsed;
+	printf("\n");
+	return (double)time / CLOCKS_PER_SEC;
 }
 
 double benchmarkx86_64(int n, float A, float* X, float* Y, float* Z) {
 	printf("x86-64: ");
+
 	clock_t time = clock();
 	SAXPYasm(n, A, X, Y, Z);
 	time = clock() - time;
-	printZ(n, Z); // temporary, for checking output
-	double timeElapsed = (double)time / CLOCKS_PER_SEC;
-	printf("|  Time Elapsed: %.4f\n", timeElapsed);
 
-	return timeElapsed;
+	printFirstTen(Z, n); // temporary, for checking output
+	printf("\n");
+	return (double)time / CLOCKS_PER_SEC;
+}
+
+void computeAverageTime(int testCaseNum, int n, float A, float* X, float* Y, float* Z) {
+	double cTimes = 0.0;
+	double x86_64Times = 0.0;
+
+	printf("CASE %d\n------------------------\n", testCaseNum);
+	for (int i = 0; i < numTestRuns; i++) {
+		printf("Run %d\n", i + 1);
+		cTimes += benchmarkC(n, A, X, Y, Z);
+		resetZ(n, Z);
+		x86_64Times += benchmarkx86_64(n, A, X, Y, Z);
+		printf("\n");
+	}
+
+	printf("Total Times:\n    C     : %.4f\n    x86-64: %.4f\n", cTimes, x86_64Times);
+	cTimes = cTimes / numTestRuns;
+	x86_64Times = x86_64Times / numTestRuns;
+	printf("Average Times:\n    C     : %.4f\n    x86-64: %.4f\n\n", cTimes, x86_64Times);
 }
 
 void testCase0() {
@@ -60,24 +81,37 @@ void testCase0() {
 	double cTimes = 0.0;
 	double x86_64Times = 0.0;
 
-	printf("CASE 0\n------------------------\n");
-	for (int i = 0; i < numTestRuns; i++) {
-		printf("Run %d\n", i + 1);
-		cTimes += benchmarkC(n, A, X, Y, Z);
-		resetZ(n, Z);
-		x86_64Times += benchmarkx86_64(n, A, X, Y, Z);
-		printf("\n");
+	computeAverageTime(0, n, A, X, Y, Z);
+}
+
+void testCase1() {
+	int n = 1048576; // 2^20
+	float A = 1.0;
+
+	float* X = (float*)malloc(n * sizeof(float)); // heap allocate to avoid stack overflow
+	float* Y = (float*)malloc(n * sizeof(float));
+	float* Z = (float*)malloc(n * sizeof(float));
+
+	if (X == NULL || Y == NULL || Z == NULL) {
+		return;
 	}
 
-	printf("Total Times: %.4f  %.4f\n", cTimes, x86_64Times);
-	cTimes = cTimes / numTestRuns;
-	x86_64Times = x86_64Times / numTestRuns;
-	printf("Average Times: %.4f  %.4f\n", cTimes, x86_64Times);
+	for (int i = 0; i < n; i++) {
+		X[i] = (float)(i % 10000);
+		Y[i] = (float)(i % 10000);
+		Z[i] = 0.0;
+	}
+
+	computeAverageTime(1, n, A, X, Y, Z);
+
+	free(X);
+	free(Y);
+	free(Z);
 }
 
 int main() {
-
 	testCase0();
+	testCase1();
 
 	return 0;
 }
